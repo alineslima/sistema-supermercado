3924796bb41bfde87a397ea5abc6bc8b92274f6c
alineslima <alineslima@outlook.com>
2020-11-24 10:50:34 -0300

    sistema de supermercado

---
+++ src/main/java/br/com/aline/sistemasupermercado/SistemaSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.aline.sistemasupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSupermercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aline/sistemasupermercado/SistemaSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aline.sistemasupermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaSupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
